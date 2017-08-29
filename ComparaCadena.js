300c3b29cb38cf535d5fd2360cf136ce24c3c244
Carlos Ulises Ocampo Silva <enlil.overlord@gmail.com>
2017-08-29 01:19:16 -0500

    Creamos ComparaCadena.js
    Este js se utiliza para ejecutar los métodos de comparación de JS

---
+++ ComparaCadena.js
@@ -0,0 +1,33 @@
+function cadena(){
+var numero = document.getElementById('numero').value;
+    var numero2 = document.getElementById('numero2').value;
+    var numero3 = document.getElementById('numero3');
+    
+   if (  numero ==  numero2)
+   {
+       document.getElementById("resp").value="Son iguales";
+      
+      }else{
+           
+        document.getElementById("resp").value="no son iguales";
+      }
+var texto = " Y aquí hay un ejemplo de como sería el metodo\n"+
+           " if (" + numero + "==" + numero2+")"+
+          " { \n "+
+          "document.write ('son iguales')\n ";
+  
+  numero3.value = texto;
+}
+
+
+
+
+function split(){
+    var numero = document.getElementById('numero').value;
+    var numero2 = document.getElementById('numero2').value;
+    var numero3 = document.getElementById('numero').value;
+    var numero4 = document.getElementById('numero2').value;
+    var numero5 = document.getElementById('numero').value;
+    var numero6 = document.getElementById('numero2');
+    
+}
